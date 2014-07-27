b0318e828624df63cb0e372b452b5ce42481a908
Toshiyuki Hina <thina@yourinventit.com>
2014-07-27 10:09:23 +0900

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ssWebConsoleFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ssWebConsoleFrontendApp
+ */
+angular.module('ssWebConsoleFrontendApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ssWebConsoleFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ssWebConsoleFrontendApp
+ */
+angular.module('ssWebConsoleFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
